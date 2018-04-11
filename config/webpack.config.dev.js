b1a55b771a75d5016d72647ceb0d9226aeb2eefc
ZhangLynn <zl8312070@sina.com>
2018-04-11 23:37:50 +0800

    配置antd引入样式

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -144,7 +144,12 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+                plugins: [
+                    // 引入样式为 css
+                    ['import', { libraryName: 'antd', style: 'css' }],
+                    // 改动: 引入样式为 less
+                    //  ['import', { libraryName: 'antd', style: true }],
+                ],
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
