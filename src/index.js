51302be8600bb9dafa99c95a9acc9e396784ffd8
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-07-06 17:03:00 +0800

    增加注释

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Routes from './router';
 import store from './store'
 import { Provider } from 'react-redux';
+//资源缓存插件 在生产环境中为用户在本地创建一个service worker 来缓存资源到本地，提升应用的访问速度
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,13 @@ import {
     Link,
     Redirect,
     Switch
-} from 'react-router-dom'
-import MyLayout from 'bundle-loader?lazy&name=[name]!../layout/layout'
+} from 'react-router-dom';
+//配置antd组件的中文
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+//路由的按需加载
 import lazyLoad from '../utils/lazyLoad/lazyLoad';
+import MyLayout from 'bundle-loader?lazy&name=[name]!../layout/layout'
 const page404 = ()=>(
     <div>404</div>
 )
@@ -17,13 +21,16 @@ const login=()=>(
 class Routes extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/login"  component={login}/>
-                    <Route path='/404' component={page404}/>
-                    <Route path="/"  component={lazyLoad(MyLayout)}/>
-                </Switch>
-            </BrowserRouter>
+            // 将antd组件配置为中文
+            <LocaleProvider locale={zhCN}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/login"  component={login}/>
+                        <Route path='/404' component={page404}/>
+                        <Route path="/"  component={lazyLoad(MyLayout)}/>
+                    </Switch>
+                </BrowserRouter>
+            </LocaleProvider>
         )
     }
 }
