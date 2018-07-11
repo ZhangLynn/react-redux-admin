4996b33dff6774c935cfa6e1fd04f2450c9f670d
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-07-11 17:40:17 +0800

    refactor : 重构router文件名称

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from './router';
+import Router from './router';
 import store from './store'
 import { Provider } from 'react-redux';
 //资源缓存插件 在生产环境中为用户在本地创建一个service worker 来缓存资源到本地，提升应用的访问速度
@@ -9,7 +9,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <Router />
     </Provider>, document.getElementById('root')
 );
 registerServiceWorker();
--- src/router.js
+++ src/router.js
@@ -10,15 +10,15 @@ import {
 import { LocaleProvider } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 //路由的按需加载
-import lazyLoad from '../utils/lazyLoad/lazyLoad';
-import MyLayout from 'bundle-loader?lazy&name=[name]!../layout/layout'
+import lazyLoad from './utils/lazyLoad/lazyLoad';
+import MyLayout from 'bundle-loader?lazy&name=[name]!./layout/layout'
 const page404 = ()=>(
     <div>404</div>
 )
 const login=()=>(
     <h1>登录页</h1>
 )
-class Routes extends Component {
+class Router extends Component {
     render() {
         return (
             // 将antd组件配置为中文
