26a838ac71ba543439f55162a6b3db9487e8ce66
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-07-12 22:24:00 +0800

    feat : 根据菜单config数据 动态生成菜单

--- src/components/SiderMenu/SiderMenu.js
+++ src/components/SiderMenu/SiderMenu.js
@@ -2,17 +2,42 @@
  * Created by ZhangLynn on 2018/7/11
  **/
 import React,{Component} from 'react';
-import {Menu, Layout} from 'antd';
+import {Menu, Layout ,Icon} from 'antd';
 import {Link} from 'react-router-dom';
 const SubMenu = Menu.SubMenu;
 const {Sider} = Layout;
 
 export default class SiderMenu extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.menus=props.menuData;
         this.state = {
             collapsed: false,
         };
+    }
+    getNavMenuItem=(menuData)=>{
+        return menuData.map(item=>{
+            return <Menu.Item key={item.path}>{this.getMenuItemPath(item)}</Menu.Item>;
+        })
+    }
+    getMenuItemPath=item=>{
+        const icon=this.getIcon(item.icon)
+        return (
+            <Link
+                to={item.path}
+                // target={target}
+                // replace={item.path === this.props.location.pathname}
+            >
+                {icon}
+                <span>{item.name}</span>
+            </Link>
+        );
+    }
+    getIcon=icon=>{
+        if(typeof icon === 'string'){
+            return <Icon type={icon} />;
+        }
+        return icon
     }
     onCollapse = (collapsed) => {
         this.setState({collapsed});
@@ -25,16 +50,14 @@ export default class SiderMenu extends Component{
                 onCollapse={this.onCollapse}
             >
                 <Menu theme="dark"
-                      openKeys={["sub1"]}
                       mode="inline">
-                    <SubMenu key="sub1" title={<span><span>菜单栏</span></span>}>
-                        <Menu.Item key="formDemo">
-                            <Link to='/todoApp'>TodoApp</Link>
-                        </Menu.Item>
-                        <Menu.Item key="tableDemo">
-                            <Link to='/tableDemo'>Table</Link>
-                        </Menu.Item>
-                    </SubMenu>
+                    {/*<Menu.Item key="formDemo">*/}
+                        {/*<Link to='/todoApp'>TodoApp</Link>*/}
+                    {/*</Menu.Item>*/}
+                    {/*<Menu.Item key="tableDemo">*/}
+                        {/*<Link to='/tableDemo'>Table</Link>*/}
+                    {/*</Menu.Item>*/}
+                    {this.getNavMenuItem(this.menus)}
                 </Menu>
             </Sider>
         )
