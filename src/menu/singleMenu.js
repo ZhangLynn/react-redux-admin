1d9b5ca88670832f2f53c77235b6d206d11bcdeb
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-07-12 22:23:14 +0800

    feat : 新增singleMenu 菜单config数据配置

---
+++ src/menu/singleMenu.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ZhangLynn on 2018/7/12
+ **/
+const singleMenuData=[
+    {
+        name:'index',
+        path:'/',
+        icon:'user',
+        authority:'admin'
+    }
+];
+
+export default singleMenuData
