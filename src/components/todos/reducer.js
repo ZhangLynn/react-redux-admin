201438b19f3bddbb8232e8e06e5b9d74d1114799
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-07-18 23:16:52 +0800

    chore : 使用redux-actions中的handleActions函数处理reducer

--- src/components/todos/reducer.js
+++ src/components/todos/reducer.js
@@ -1,33 +1,23 @@
 import {ADD_TODO, TOGGLE_TODO, REMOVE_TODO}from './actionTypes.js';
+import {handleActions} from 'redux-actions';
 
-export default (state = [], action) => {
-  switch(action.type) {
-    case ADD_TODO: {
-      return [
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        },
-        ...state
-      ]
+export default handleActions({
+    [ADD_TODO] :(state=[], action) => {
+        return [
+            ...state,
+            action.payload
+        ]
+    },
+    [TOGGLE_TODO] :(state=[], action) => {
+        return state.map(todoItem=>{
+            if(action.payload.id===todoItem.id){
+                return {...todoItem, completed: !todoItem.completed};
+            }else{
+                return todoItem
+            }
+        })
+    },
+    [REMOVE_TODO] :(state=[], action) => {
+        return state.filter(todoItem=>(todoItem.id!==action.payload.id))
     }
-    case TOGGLE_TODO: {
-      return state.map((todoItem) => {
-        if (todoItem.id === action.id) {
-           return {...todoItem, completed: !todoItem.completed};
-        } else {
-          return todoItem;
-        }
-      })
-    }
-    case REMOVE_TODO: {
-      return state.filter((todoItem) => {
-        return todoItem.id !== action.id;
-      })
-    }
-    default: {
-      return state;
-    }
-  }
-}
+},[]);
