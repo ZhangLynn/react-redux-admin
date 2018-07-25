a2515be2b5f7a84835078ce802a75bc238da2679
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-07-25 20:00:10 +0800

    style: 删除注释 调整空格

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ import { Provider } from 'react-redux';
 //资源缓存插件 在生产环境中为用户在本地创建一个service worker 来缓存资源到本地，提升应用的访问速度
 import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <AppContainer>
-//             <Router />
-//         </AppContainer>
-//     </Provider>, document.getElementById('root')
-// );
+//热更新实现
 const render = Component => {
     ReactDOM.render(
         //绑定redux、热加载
@@ -28,24 +22,12 @@ const render = Component => {
 }
 
 render(Router);
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <AppContainer>
-//             <Router />
-//         </AppContainer>
-//     </Provider>, document.getElementById('root')
-// );
+
 // Webpack Hot Module Replacement API
 if (module.hot) {
     module.hot.accept('./router', () => {
         render(Router);
     })
 }
-// if (module.hot) {
-//     module.hot.accept('./router', () => { // 当我们热更新的代码出现的时候，把App重新加载
-//         const NextApp = require('./router').default //因为在App里使用的是export default语法，这里使用的是require,默认不会加载default的，所以需要手动加上
-//         render(NextApp) // 重新渲染到 document 里面
-//     })
-// }
 
 registerServiceWorker();
