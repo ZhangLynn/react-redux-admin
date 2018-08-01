71b52609169bbe8626ba759d9fcd7488d910d90a
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-08-01 22:31:20 +0800

    docs: 添加启动脚本的中文注释

--- scripts/start.js
+++ scripts/start.js
@@ -1,22 +1,22 @@
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
+// 这是第一件事, 设置node的运行环境为 'development', 可以让运行代码知道它正确的环境
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+// 使脚本在发生不能处理的rejections时崩溃, 而不是不报错去无视他们。
+// 在未来，未经处理的承诺rejections将会使用非零退出代码, 终止Node.js进程.
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
+// 确保读取到环境变量
 require('../config/env');
 
 const fs = require('fs');
 const chalk = require('chalk');
 const webpack = require('webpack');
+//
 const WebpackDevServer = require('webpack-dev-server');
 const clearConsole = require('react-dev-utils/clearConsole');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
