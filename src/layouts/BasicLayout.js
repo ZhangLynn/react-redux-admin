4490846feeef37e609296bddf7ae9d0b8654de65
WINDOWS-PNVU2BF\ZhangLynn <zl8312070@sina.com>
2018-08-06 21:30:24 +0800

    style: 删除import()异步加载方式

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
 import SiderMenu from '../components/SiderMenu'
-// import AsyncComponent from '../utils/AsyncLoad/AsyncComponent';
 import Loadable from 'react-loadable';
+import {
+    Route,
+    Switch
+} from 'react-router-dom';
+import style from './BasicLayout.less'
 import MyLoadingComponent from '../utils/MyLoadingComponent';
 const TodoApp = Loadable({
     loader: () => import("../pages/todoapp"),
@@ -12,13 +16,7 @@ const TableDemo = Loadable({
     loader: () => import("../testComponent/tableDemo"),
     loading: MyLoadingComponent
 });
-// const TodoApp = AsyncComponent(() => import("../pages/todoapp"));
-// const TableDemo = AsyncComponent(() => import("../testComponent/tableDemo"));
-import {
-    Route,
-    Switch
-} from 'react-router-dom';
-import style from './BasicLayout.less'
+
 import singleMenuData from '../menu/singleMenu'
 export default class BasicLayout extends Component {
     render() {
